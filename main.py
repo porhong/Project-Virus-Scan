1ecaff7dc68121546b56ecea57d1923e7507fc54
porhong <poehongkeat@gmail.com>
2023-11-01 17:17:33 +0700

    V1.0.2(dev) - Update result - Update Response tg

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import requests
 import json
-import url_scan_API
+from url_scan_API import result_control
 from aiogram import Dispatcher, Bot, executor, types
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from aiogram.dispatcher.filters import Text
@@ -34,15 +34,19 @@ async def hello(message: types.Message):
 
 @dp.message_handler(content_types='text')
 async def check_link(message: types.Message):
+    await message.answer_sticker("CAACAgIAAxkBAAEnSpZlQhedfApj13TDMdyBPy0Iw5it3gACXwAD29t-AAGEsFSbEa7K4zME")
+    message_id = message.message_id
     link = message.text
     validation = validators.url(link)
     if validation:
         # Url Scan
-        result_list = url_scan_API.get_link_result(
-            url_scan_API.post_link(link))
+        result_list = result_control(link)
         harmless = result_list["Short_result"]["harmless"]
         malicious = result_list["Short_result"]["malicious"]
         suspicious = result_list["Short_result"]["suspicious"]
+        treat = result_list["treat_status"]
+        treat_count = len(treat)
+        print(treat)
         total_scan = harmless + malicious + suspicious
         try:
             s = (harmless / total_scan) * 100
@@ -57,7 +61,9 @@ async def check_link(message: types.Message):
         except ZeroDivisionError:
             d = 0
 
-        if w > 30:
+        if treat_count > 0:
+            result = "Danger"
+        elif w > 10:
             result = "Warning"
         elif d > 10:
             result = "Danger"
@@ -67,34 +73,63 @@ async def check_link(message: types.Message):
             case"Clean":
                 response_title = "<b>តំណភ្ជាប់មានសុវត្ថិភាព 🟩 </b>"
                 response_note = "អ្នកអាចប្រើប្រាស់តំណភ្ជាប់នេះដោយសុវត្តិភាព។"
+                response_body = f"""
+ - មានសុវត្ថិភាព : {s:.1f}%
+ - មានភាពសង្ស័យ​ : {w:.1f}%
+"""
+
             case"Warning":
                 response_title = "<b>តំណភ្ជាប់គួរអោយសង្ស័យ 🟨 </b>"
                 response_note = "សូមធ្វើការប្រើប្រាស់តំណភ្ជាប់នេះដោយមានការប្រុងប្រយ័ត្ន។"
+                response_body = f"""
+ - មានសុវត្ថិភាព : {s:.1f}%
+ - មានភាពសង្ស័យ​ : {w:.1f}%
+"""
             case"Danger":
                 response_title = "<b>តំណភ្ជាប់មានគ្រោះថ្នាក់ 🟥 </b>"
-                response_note = "អ្នកមិនគួរបើកតំណភ្ជាប់នេះទេ ដោយតំណភ្ជាប់នេះអាចនឹងមានផ្ទុកនូវមេរោគ ឫការក្លែងបន្លំផ្សេងៗ។"
+                response_note = "អ្នកមិនគួរបើកតំណភ្ជាប់នេះទេ ដោយតំណភ្ជាប់នេះអាចនឹងមានផ្ទុកនូវមេរោគ ឫការក្លែងបន្លំផ្សេងៗដែលអាចបង្កគ្រោះថ្នាក់។"
+                response_body = f"""
+ តំណភ្ចាប់នេះត្រូវបានរកឃើញការគំរាមកំហែងប្រភេទ : <strong>{treat[0].upper()}</strong>
+"""
         response_message = f"""{response_title}
 
 <b>ពត៌មានសង្ចេប</b>
-
-    - មានសុវត្ថិភាព : {s:.1f}%
-    - គួរឱ្យសង្ស័យ​ : {w:.1f}%
-    - មានគ្រោះថ្នាក់ : {d:.1f}%
-
+{response_body}
 <b>ការសន្និដ្ឋាន</b>
 
-    {response_note}
+{response_note}
 
 <b>តំណភ្ជាប់</b>
+        
 
-    {link}"""
-        await message.answer(response_message, parse_mode="html")
+{link}"""
+
+        response_message_error = f"""{response_title}
+
+<b>ពត៌មានសង្ចេប</b>
+{response_body}
+<b>ការសន្និដ្ឋាន</b>
+
+{response_note}
+
+"""
+        if result == "Clean":
+            await message.answer(response_message, parse_mode="html")
+            await message.answer_sticker("CAACAgIAAxkBAAEnSyhlQiDZxwKnbTLSy8iPIBX-GO-igAACPwAD29t-AAH05pw4AeSqaTME")
+        elif result == "Warning":
+            await message.answer(response_message, parse_mode="html")
+            await message.answer_sticker("CAACAgIAAxkBAAEnSyRlQiCzBcL_AfdhB4kkk_AxX0LjngACbQAD29t-AAF1HuyF8vtEpTME")
+        else:
+            await message.answer(response_message_error, parse_mode="html")
+            await message.answer_sticker("CAACAgIAAxkBAAEnSyBlQiAukwvBftbkAifBWLsogqYXIwACcAAD29t-AAHqAAG3tyaYON0zBA")
+
     else:
         await message.answer(f"""<b>តំណភ្ជាប់មិនត្រឺមត្រូវ ⚠ </b>
                              
 <pre>{link}</pre>
 
 សូមធ្វើការពិនិត្រទៅលើតំណភ្ជាប់ខាងលើ រួចព្យាយាមម្ដងទៀត។""", parse_mode="html")
+        await message.answer_sticker("CAACAgIAAxkBAAEnSx5lQiAJQT6t3EVFlwzUSlLgFU-IFAACYwAD29t-AAGMnQU950KD5zME")
 
 
 async def main() -> None:
@@ -102,4 +137,4 @@ async def main() -> None:
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
-  executor.start_polling(dp)
+    executor.start_polling(dp)
--- url_scan_API.py
+++ url_scan_API.py
@@ -1,7 +1,10 @@
 import requests
 import json
+import time
+import operator
 
 url = "https://www.virustotal.com/api/v3/urls"
+treat_url = "https://www.virustotal.com/api/v3/popular_threat_categories"
 headers = {
     "accept": "application/json",
     "x-apikey": "2162a3224e1b3688e7daf893f9178caa72699e6ba3231214276816181410314c",
@@ -20,22 +23,40 @@ def post_link(link_check):
 
 def get_link_result(result_link):
     response = requests.get(result_link, headers=headers)
+    threat_categories = requests.get(treat_url, headers=headers)
+    threat_categories = json.loads(threat_categories.text)
     result = response.text
-    print(result)
+    # print(result)
     result = json.loads(result)
     result_after_scan = {}
     result_release = {}
+    result_treat = []
     antiVirusCompany = list(result["data"]["attributes"]["results"].keys())
-    # for item in antiVirusCompany:
-    #     result_by_company = result["data"]["attributes"]["results"][item]["engine_name"]
-    #     result_confirm_by_company = result["data"]["attributes"]["results"][item]["result"]
-
-    #     result_after_scan[result_by_company] = result_confirm_by_company
+    treat = list(threat_categories["data"])
+    for item in antiVirusCompany:
+        result_by_company = result["data"]["attributes"]["results"][item]["engine_name"]
+        result_confirm_by_company = result["data"]["attributes"]["results"][item]["result"]
+        if result_confirm_by_company in treat:
+            result_treat.append(result_confirm_by_company)
+    result_treat = list(set(result_treat))
     result_summary = result["data"]["attributes"]["stats"]
+    result_status = result["data"]["attributes"]["status"]
     result_release["Short_result"] = result_summary
+    result_release["result_status"] = result_status
+    result_release["treat_status"] = result_treat
     return result_release
 
 
-result = get_link_result(post_link(
-    "https://github.com/porhong/Project-Badbot.YT/blob/main/tt.py"))
-print(result)
+def result_control(link):
+    request_link = post_link(link)
+    result = get_link_result(request_link)
+    result_status = result["result_status"]
+    while result_status == "queued":
+        time.sleep(1)
+        result = get_link_result(request_link)
+        result_status = result["result_status"]
+    return result
+
+
+# a = result_control("https://kraitstones.help/")
+# print(a)
